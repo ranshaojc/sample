bfe68dff68b4d231dcd8f7c37b9c65f2bccf51d0
jc <jc@test.com>
2018-09-18 08:08:34 +0000

    Fix bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,17 +25,6 @@ class UsersController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
-        return;
-    }
-
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required|max:50',
-            'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
-        ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
